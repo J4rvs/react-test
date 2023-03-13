0efea087f988b4ed1dbe3dbec60fed3f9e0bc12e
Wilfredo González Carvajal <wilfredogonzalezcarvajal@gmail.com>
2023-03-13 11:36:25 -0400

    complete about

--- src/components/sections/About/About.js
+++ src/components/sections/About/About.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import Figure from 'react-bootstrap/Figure';
+import { Container } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './About.css';
+
 function About() {
     return (
-        <div>
-            <Figure>
-                <Figure.Image
-                width={171}
-                height={180}
-                alt="171x180"
-                src="holder.js/171x180"
-                />
-            </Figure>
-            <h1>We Help Businesses</h1>
-            <h1>Grow and Innovate</h1>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
-        </div>
+        <section id='about'>
+            <Container fluid>
+                <svg width="100vh" height="500px">
+                    <rect x='0' y='0' width="32px" height="390px" fill='#DB7093'/>
+                </svg>
+                <Row>
+                    <Col className='about-text' md={{ span: 6, offset: 3}}>
+                        <h1>We Help Businesses</h1>
+                        <h1 style={{color: '#DB7093'}}>Grow and Innovate</h1>
+                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+                    </Col>
+                </Row>
+                
+            </Container>
+        </section>
     );
 }
 
